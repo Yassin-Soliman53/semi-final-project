226a83a1f5c6d9cc2110ddbabf8da50a873e0105
Yassin Soliman <yassin.soliman.120@gmail.com>
2026-08-24 21:06:07 +0300

    idk the error but it is a try

--- feed-back.js
+++ feed-back.js
@@ -1,5 +1,5 @@
 import { collection, addDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
-import { db } from "./firebae"; 
+import { db } from "./firebae.js"; 
 
 const form = document.getElementById("feedbackForm");
 const submitBtn = document.getElementById("submitBtn");
