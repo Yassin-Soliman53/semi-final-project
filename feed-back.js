b21887e2730a3adff1343cdf89659ddbf41236d4
Yassin Soliman <yassin.soliman.120@gmail.com>
2026-08-24 18:18:48 +0300

    the firebase error fix

--- feed-back.js
+++ feed-back.js
@@ -1,34 +1,22 @@
-// Import Firebase SDK modules directly via CDN
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getFirestore, collection, addDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
-import { db } from "./firebae.js";
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAI-MoNiKAk8I-wnXEUzPTFwujxVpRC5FI",
-  authDomain: "semifinal-3b372.firebaseapp.com",
-  projectId: "semifinal-3b372",
-  storageBucket: "semifinal-3b372.firebasestorage.app",
-  messagingSenderId: "705718818557",
-  appId: "1:705718818557:web:b2ccd0d860ea0cf6485094",
-  measurementId: "G-TZ7EJ1FX1V"
-};
+import { collection, addDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+import { db } from "./firebase.js";
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
 const form = document.getElementById("feedbackForm");
 const submitBtn = document.getElementById("submitBtn");
+
 form.addEventListener("submit", async (e) => {
-    e.preventDefault(); 
+    e.preventDefault();
     submitBtn.disabled = true;
     submitBtn.innerText = "Submitting...";
+
     const firstName = document.getElementById("fname").value;
     const lastName = document.getElementById("lname").value;
     const feedbackText = document.getElementById("feedback").value;
 
     try {
         const docRef = await addDoc(collection(db, "feedbacks"), {
-            firstName: firstName,
-            lastName: lastName,
+            firstName,
+            lastName,
             feedback: feedbackText,
             createdAt: serverTimestamp()
         });
